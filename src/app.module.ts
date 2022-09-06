47b54c265256dae6f052884c55bca46ae67bd37a
Victor Daniel Quino Jimenez <victordanielquino@gmail.com>
2022-09-06 15:52:52 -0400

    add: rabitmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ADMIN_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://root:pass@localhost:5672'],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject('ADMIN_SERVICE') private readonly client: ClientProxy) {}
+
+  findOne(id: string): any {
+    this.client.emit('findOne', id);
+    return `Hello World! ${id}`;
   }
 }
